048dad1d6467a5ed2e5fb77064260f8a6a517969
Renald <r.kronbergs@voco.ee>
2026-04-06 08:46:52 +0300

    Lab 2.3-Singleton

---
+++ Lab_2_3/Singleton.py
@@ -0,0 +1,29 @@
+def is_singleton(factory):
+    obj1 = factory()
+    obj2 = factory()
+    return obj1 is obj2
+
+
+class Database:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
+def create_db():
+    return Database()
+
+
+class User:
+    pass
+
+
+def create_user():
+    return User()
+
+
+print(is_singleton(create_db))
+print(is_singleton(create_user))
