12c063383bada35371eb3fca40c6d66840fcc8b8
Kasutaja <r.kronbergs@voco.ee>
2026-04-26 16:06:36 +0300

    Lab_3_2.py-Adapter

---
+++ Lab_3_2/Adapter.py
@@ -0,0 +1,11 @@
+class SquareToRectangleAdapter:
+    def __init__(self, square):
+        self.square = square
+
+    @property
+    def width(self):
+        return self.square.side
+
+    @property
+    def height(self):
+        return self.square.side
